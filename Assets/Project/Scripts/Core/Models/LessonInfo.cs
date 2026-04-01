95a6e7592f069fa39755011823517eca8f112535
natuan05 <160258271+natuan05@users.noreply.github.com>
2026-04-01 14:53:44 +0700

    Delete imported assets + Add Core\Models

---
+++ Assets/Project/Scripts/Core/Models/LessonInfo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public enum LessonType
+{
+    theoretical,
+    practical
+}
+[CreateAssetMenu(fileName = "LessonInfo", menuName = "LessonInfo")]
+public class LessonInfo : ScriptableObject
+{
+    public string lesson_name;
+    public string level_name;
+    public int lesson_index;
+    public int level_index;
+    public string lesson_id;
+    public string level_id;
+    public LessonType type;
+}
